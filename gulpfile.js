10b7e4677113417da4d3d5e96b77ca6ba0ea4fcf
Lanz <zihao.lan@dianrong.com>
2015-11-13 17:33:25 +0800

    format code & make it works

--- gulpfile.js
+++ gulpfile.js
@@ -17,93 +17,93 @@ var buffer = require('vinyl-buffer');
 var browserify = require('browserify');
 
 var AUTOPREFIXER_BROWSERS = [
-	'last 3 versions',
-	'ie >= 8',
-	'ios >= 7',
-	'android >= 4.4',
-	'bb >= 10'
+  'last 3 versions',
+  'ie >= 8',
+  'ios >= 7',
+  'android >= 4.4',
+  'bb >= 10'
 ];
 
-gulp.task('clean', function() {
-	return gulp.src([
-		'./public/build/css/**/*.css',
-		'./public/build/js/**/*.js'],
-		{read: false})
-		.pipe(clean());
+gulp.task('clean', function () {
+  return gulp.src([
+      './public/build/css/**/*.css',
+      './public/build/js/**/*.js'],
+    {read: false})
+    .pipe(clean());
 });
 
-gulp.task('img', function() {
-	return gulp.src('./public/images/*.{gif,jpg,png}')
-		.pipe(imagemin({
-			progressive: true,
-			interlaced: true,
-			svgoPlugins: [ {removeViewBox:false}, {removeUselessStrokeAndFill:false} ]
-		}))
-		.pipe(gulp.dest('./public/build/images/'))
+gulp.task('img', function () {
+  return gulp.src('./public/images/*.{gif,jpg,png}')
+    .pipe(imagemin({
+      progressive: true,
+      interlaced: true,
+      svgoPlugins: [{removeViewBox: false}, {removeUselessStrokeAndFill: false}]
+    }))
+    .pipe(gulp.dest('./public/build/images/'))
 });
 
 gulp.task('images', ['img']);
 
-gulp.task('css', function() {
-	return gulp.src('./public/less/main.less')
-		.pipe(less({}))
-		.pipe(gulp.dest('./public/build/css/'))
-		.pipe(autoprefixer(AUTOPREFIXER_BROWSERS))
-		.pipe(rename({ suffix: '.min' }))
-		.pipe(minifycss())
-		.pipe(gulp.dest('./public/build/css/'))
-		.pipe(browserSync.stream({match: '**/*.css'}))
+gulp.task('css', function () {
+  return gulp.src('./public/less/main.less')
+    .pipe(less({}))
+    .pipe(gulp.dest('./public/build/css/'))
+    .pipe(autoprefixer(AUTOPREFIXER_BROWSERS))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifycss())
+    .pipe(gulp.dest('./public/build/css/'))
+    .pipe(browserSync.stream({match: '**/*.css'}))
 });
 
-gulp.task('jslint', function() {
-	return gulp.src('./public/javascripts/**/*.js')
-		.pipe(jshint())
-		.pipe(jshint.reporter('default'))
+gulp.task('jslint', function () {
+  return gulp.src('./public/javascripts/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
 });
 
-gulp.task('scripts', function() {
-	return gulp.src('./public/javascripts/**/*.js')
-		.pipe(concat('all.js'))
-		.pipe(gulp.dest('./public/build/js/'))
-		.pipe(rename('all.min.js'))
-		.pipe(stripdebug())
-		.pipe(uglify())
-		.pipe(gulp.dest('./public/build/js/'))
-		.pipe(browserSync.stream({match: '**/*.js'}))
+gulp.task('scripts', function () {
+  return gulp.src('./public/javascripts/**/*.js')
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest('./public/build/js/'))
+    .pipe(rename('all.min.js'))
+    .pipe(stripdebug())
+    .pipe(uglify())
+    .pipe(gulp.dest('./public/build/js/'))
+    .pipe(browserSync.stream({match: '**/*.js'}))
 });
 
-gulp.task('allScripts', function() {
-	var b = browserify({
-		entries: "./public/javascripts/muzic.bootstrap.js",
-		basedir: '.'
-	});
+gulp.task('allScripts', function () {
+  var b = browserify({
+    entries: "./public/javascripts/muzic.bootstrap.js",
+    basedir: '.'
+  });
 
-	return b.bundle()
-		.pipe(source('muzic.bootstrap.js'))
-		.pipe(buffer())
-		.pipe(gulp.dest("./public/build/"));
+  return b.bundle()
+    .pipe(source('muzic.bootstrap.js'))
+    .pipe(buffer())
+    .pipe(gulp.dest("./public/build/"));
 });
 
 //- TODO: Append file hash in jade files
 //var timestamp = new Date().getTime();
 
 gulp.task('watch', function () {
-	// proxy local server
-	browserSync.init({
-	  port: 3000,
-	  proxy: 'localhost:8080'
-	});
+  // proxy local server
+  browserSync.init({
+    port: 3000,
+    proxy: 'localhost:8080'
+  });
 
-	gulp.start(['img', 'css', 'allScripts']);
+  gulp.start(['img', 'css', 'allScripts']);
 
-	gulp.watch('./views/**/*.jade').on('change', reload);
-	gulp.watch('./public/less/**/*', ['css']);
-	gulp.watch('./public/javascripts/**/*', ['jslint', 'allScripts']).on('change', reload);
+  gulp.watch('./views/**/*.jade').on('change', reload);
+  gulp.watch('./public/less/**/*', ['css']);
+  gulp.watch('./public/javascripts/**/*', ['jslint', 'allScripts']).on('change', reload);
 });
 
 gulp.task('build', ['clean', 'img', 'css', 'jslint', 'scripts']);
 
-gulp.task('default', ['clean'], function() {
-	gulp.start('css', 'jslint', 'scripts');
+gulp.task('default', ['clean'], function () {
+  gulp.start('css', 'jslint', 'scripts');
 });
 
--- public/javascripts/controllers/lib/music.js
+++ public/javascripts/controllers/lib/music.js
@@ -1,136 +1,144 @@
 var angular = require('angular');
 
-var mainModule = angular.module('MUZIC.ctrl.main', []);
+var mainModule = angular.module('mainModule', []);
 
-mainModule.controller('mainCtrl', [function($scope) {
-	var playAudio = document.getElementById('player');
-	var	cd = $('#cd');
-	var	ins = $('#in');
-	var	album = $('#album');
-	var	cover = $('#in');
-	var	cover_out = $('.out');
-	var	play = $('#play');
-	var	music_name = $('#music_name');
-	var	artist = $('#artist');
-	var	music_lrc = $('#music_lrc');
+mainModule.controller('mainCtrl', [
+  '$scope',
+  function ($scope) {
+    var playAudio = document.getElementById('player');
+    var cd = $('#cd');
+    var ins = $('#in');
+    var album = $('#album');
+    var cover = $('#in');
+    var cover_out = $('.out');
+    var play = $('#play');
+    var music_name = $('#music_name');
+    var artist = $('#artist');
+    var music_lrc = $('#music_lrc');
 
-	$(window).resize(function () {
-		cdResize();
-	});
+    $(window).resize(function () {
+      cdResize();
+    });
 
-	$("#player").bind("ended", function () {
-		$scope.nextMusic();
-	});
+    $("#player").bind("ended", function () {
+      $scope.nextMusic();
+    });
 
-	ins.one('click', function() {
-		if(cover.css('opacity') === '0') {
-			cover.css('opacity', '1');
-			$('.action').css('opacity', '1');
-			album.css({
-				'background-color': 'white',
-				'border': '1px solid #9ad2fe',
-				'background-repeat': 'no-repeat',
-				'background-size': 'cover',
-				'background-position': '50% 50%'
-			});
-		}
+    ins.one('click', function () {
+      if (cover.css('opacity') === '0') {
+        cover.css('opacity', '1');
+        $('.action').css('opacity', '1');
+        album.css({
+          'background-color': 'white',
+          'border': '1px solid #9ad2fe',
+          'background-repeat': 'no-repeat',
+          'background-size': 'cover',
+          'background-position': '50% 50%'
+        });
+      }
 
-		$scope.nextMusic();
-	});
+      $scope.nextMusic();
+    });
 
-	$scope.initPlay = function() {
-		console.log('click2');
-		if(cover.css('opacity') === '0') {
-			cover.css('opacity', '1');
-			$('.action').css('opacity', '1');
-			album.css({
-				'opacity': '1',
-				'background-color': 'white',
-				'border': '1px solid #9ad2fe',
-				'background-repeat': 'no-repeat',
-				'background-size': 'cover',
-				'background-position': '50% 50%'
-			});
-		}
+    $scope.initPlay = function () {
+      console.log('click2');
+      if (cover.css('opacity') === '0') {
+        cover.css('opacity', '1');
+        $('.action').css('opacity', '1');
+        album.css({
+          'opacity': '1',
+          'background-color': 'white',
+          'border': '1px solid #9ad2fe',
+          'background-repeat': 'no-repeat',
+          'background-size': 'cover',
+          'background-position': '50% 50%'
+        });
+      }
 
-		$scope.nextMusic();
-	};
+      $scope.nextMusic();
+    };
 
-	$scope.musicPlay = function() {
-		console.log('click');
-		if (playAudio.paused) {
-			playAudio.play();
-			play.attr('src', '/build/images/pause.png');
-			album.addClass('roll');
-			cover.addClass('roll');
-		} else {
-			playAudio.pause();
-			play.attr('src', '/build/images/play.png');
-			album.removeClass('roll');
-			cover.removeClass('roll');
-		}
-	};
+    $scope.musicPlay = function () {
+      console.log('music play');
+      if (playAudio.paused) {
+        playAudio.play();
+        play.attr('src', '/build/images/pause.png');
+        album.addClass('roll');
+        cover.addClass('roll');
+      } else {
+        playAudio.pause();
+        play.attr('src', '/build/images/play.png');
+        album.removeClass('roll');
+        cover.removeClass('roll');
+      }
+    };
 
-	$scope.nextMusic = function() {
-		playAudio.pause();
-		album.removeClass('roll');
-		cover.removeClass('roll');
+    $scope.nextMusic = function () {
+      console.log('next music');
+      playAudio.pause();
+      album.removeClass('roll');
+      cover.removeClass('roll');
 
-		loadMusic();
-		play.attr('src', '/build/images/pause.png');
-	};
+      loadMusic();
+      play.attr('src', '/build/images/pause.png');
+    };
 
-	function loadMusic() {
-		$.ajax({
-			type: 'GET',
-			url: '/song/24603571/'
-		}).done(function (data) {
-			var result = data;
+    function loadMusic() {
+      $.ajax({
+        type: 'GET',
+        url: '/song/24603571/'
+      }).done(function (data) {
+        var result = data;
 
-			var cdCover = result['songs'][0]['album']['picUrl'],
-				musicName = result['songs'][0]['name'],
-				artistName = result['songs'][0]['artists'][0]['name'],
-				mp3 = result['songs'][0]['mp3Url'];
+        var cdCover = result['songs'][0]['album']['picUrl'];
+        var musicName = result['songs'][0]['name'];
+        var artistName = result['songs'][0]['artists'][0]['name'];
+        var mp3 = result['songs'][0]['mp3Url'];
 
-			$('#player').attr('src', mp3);
-			album.css({'background-image':'url( "' + cdCover + '")', 'opacity':0}).animate({opacity: 1}, 1000);
-			music_name.html(musicName);
-			artist.html(artistName);
-			playAudio.play();
-			album.addClass('roll');
-			cover.addClass('roll');
-		});
-	}
+        $('#player').attr('src', mp3);
 
-	$scope.volume = function(vol) {
-		playAudio.volume = vol / 10;
-	};
+        album.css({
+          'background-image': 'url( "' + cdCover + '")',
+          'opacity': 0
+        }).animate({opacity: 1}, 1000);
 
-	function cdResize() {
-		cdHeight = cd.height();
-		cd.css("width", cdHeight);
-	}
+        music_name.html(musicName);
+        artist.html(artistName);
+        playAudio.play();
+        album.addClass('roll');
+        cover.addClass('roll');
+      });
+    }
 
-	$scope.$watch('volumes', function(newValue, oldValue) {
-		if (newValue && oldValue) {
-			if (newValue != oldValue) {
-				$scope.volume(newValue);
-				console.log(newValue);
-			}
-		}
-	}, true);
+    $scope.volume = function (vol) {
+      playAudio.volume = vol / 10;
+    };
 
-	$(document).ready(function() {
-		cdResize();
-		$(document).keypress(function(e){
-			if ((e.which && e.which == 32) || (e.keyCode && e.keyCode == 32)) {
-				$scope.musicPlay();
-				return false;
-			} else {
-				return true;
-			}
-		});
-	});
-}]);
+    function cdResize() {
+      cdHeight = cd.height();
+      cd.css("width", cdHeight);
+    }
+
+    $scope.$watch('volumes', function (newValue, oldValue) {
+      if (newValue && oldValue) {
+        if (newValue != oldValue) {
+          $scope.volume(newValue);
+          console.log(newValue);
+        }
+      }
+    }, true);
+
+    $(document).ready(function () {
+      cdResize();
+      $(document).keypress(function (e) {
+        if ((e.which && e.which == 32) || (e.keyCode && e.keyCode == 32)) {
+          $scope.musicPlay();
+          return false;
+        } else {
+          return true;
+        }
+      });
+    });
+  }]);
 
 module.exports = mainModule;
